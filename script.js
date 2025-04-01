5c250e437810c062c56283d7450e728f81468e1a
aditya-7562 <aditya.12114424@lpu.in>
2025-04-01 14:04:26 +0530

    github action config for secrets

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = process.env.DATA_API;
+const apiKey = "";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
